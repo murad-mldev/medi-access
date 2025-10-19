4e0da48d7ba6690754067711ea9daeba9a2d89a1
murad-mldev <murad.hossain.sebpo@gmail.com>
2025-10-20 01:23:26 +0600

    Doctor project created

---
+++ src/main/java/med/doctorlink/DoctorlinkApplication.java
@@ -0,0 +1,13 @@
+package med.doctorlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/med/doctorlink/DoctorlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package med.doctorlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
